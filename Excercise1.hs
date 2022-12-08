475daaa3f896a1832f105d71243f357e7b586987
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-08 19:06:47 +0000

    Initial commit

---
+++ Excercise1.hs
@@ -0,0 +1,20 @@
+-- Question 1
+-- Write a multiline comment below.
+{-
+How to avoid swing on the top of the market
+  I am not sure.
+But you can learn day by day. And do not bet the big size
+-}
+-- Question 2
+-- Define a function that takes a value and multiplies it by 3.
+nhanvoiba a = a*3
+-- Question 3
+-- Define a function that calculates the area of a circle.
+dientichhinhtrone b = b ^ 2 * pi
+-- Question 4
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder.
+thetichhinhtru c d = c^2 * pi * d
+-- Question 5
+-- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+thetichsosanhvoi42 e f = e^2 * pi * f
+
