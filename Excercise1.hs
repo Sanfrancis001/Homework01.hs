a548ee91625efcc5a0c307bffb91c39301fc2cb9
CMPAO31 <69440677+CMPAO31@users.noreply.github.com>
2022-12-09 14:21:08 +0000

    Change excercise5

--- Excercise1.hs
+++ Excercise1.hs
@@ -7,14 +7,16 @@ But you can learn day by day. And do not bet the big size
 -}
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
-nhanvoiba a = a*3
+nhanvoiba a = a * 3
+
 -- Question 3
 -- Define a function that calculates the area of a circle.
 dientichhinhtrone b = b ^ 2 * pi
+
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder.
-thetichhinhtru c d = c^2 * pi * d
+thetichhinhtru c d = c ^ 2 * pi * d
+
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
-thetichsosanhvoi42 e f = e^2 * pi * f
-
+thetichsosanhvoi42 c d = thetichhinhtru c d >= 42
